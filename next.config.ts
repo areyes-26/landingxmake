a2d7d47313fb8a6c356417b045a77867ea23f2cc
Ariel Reyes <ariel.reyes@kuepa.com>
2025-05-30 15:08:08 +0000

    Credenciales

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /* podés agregar otras configuraciones aquí */
 };
 
 export default nextConfig;
+
